ea4da6284abefcb3f6629bfb4804ded903d4e3f5
Ericka Arguedas <ericka.arguedas@gmail.com>
2020-10-21 20:59:53 -0700

    React Router Parameters

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -1,12 +1,26 @@
 import React, { Component } from 'react';
-import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
+import { Card, CardImg, CardBody, CardText, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
+
+    function RenderCampsite({campsite}) {
+        return (
+            <div className="col-md-5 m-1">
+                <Card>
+                    <CardImg top src={campsite.image} alt={campsite.name} />
+                    <CardBody>
+                          <CardText>{campsite.description}</CardText>
+                    </CardBody>
+                </Card>
+            </div>
+
+        );
+    }
 
 
-class CampsiteInfo extends Component{
-    renderComments(comments){
-        if(comments){
-            return(
-                <div>
+    function RenderComments({comments}) {
+        if (comments) {
+            return (
+                <div className="col-md-5 m-1">
                 <h4>Comments</h4>
                 {comments.map(comment => {
                         return(
@@ -19,36 +33,31 @@ class CampsiteInfo extends Component{
                 </div>
             );
         } 
-        return <div />
-    }
-
-    renderCampsite(campsite) { //NOT HERE IN WEEK 3//
-        return(
-            <div className='col-md-5 m-1'>
-            <Card>
-                <CardImg top src={campsite.image} alt={campsite.name} />
-                <CardBody>
-                    <CardTitle>{campsite.name}</CardTitle>
-                    <CardText>{campsite.description}</CardText>
-                </CardBody>
-            </Card> 
-            </div>
-        )
+        return <div />;
     }
     
-    render() {
-        if (this.props.campsite){
+    function CampsiteInfo(props) {
+        if (props.campsite){
             return (
                 <div className="container">
                     <div className="row">
-                       {this.renderCampsite(this.props.campsite)}
-                       {this.renderComments(this.props.campsite.comments)}
+                        <div className="col">
+                           <Breadcrumb>
+                              <BreadcrumbItem><Link to="/directory">Directory</Link></BreadcrumbItem>
+                              <BreadcrumbItem active>{props.campsite.name}</BreadcrumbItem>
+                           </Breadcrumb>
+                           <h2>{props.campsite.name}</h2>
+                    <hr />
+                </div>
+            </div>
+                    <div className="row">
+                       <RenderCampsite campsite={props.campsite} />
+                       <RenderComments comments={props.comments} />
                     </div>
                 </div>
-            ); 
-        } 
-      return <div />;
+            );  
     }
+    return <div />;
   }
   
   export default CampsiteInfo;
