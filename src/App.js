c178bd7abae1750cecb00bf4ef569da4c3931b0d
Ericka Arguedas <ericka.arguedas@gmail.com>
2020-10-21 17:25:13 -0700

    Router & Adding Single Page App

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
 import Main from './components/MainComponent';
+import { BrowserRouter } from 'react-router-dom';
 import './App.css';
 
 //console.log(CAMPSITES)//
 class App extends Component { //INFO CENTER//
     render() { //WHAT GETS RENDERED//
         return (
+            <BrowserRouter>
             <div className="App">
                 <Main />
             </div>
+            </BrowserRouter>
         );
     };
 }
