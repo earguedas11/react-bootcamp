75f0924b4f179150d4b3069c0356735766a0afed
Ericka Arguedas <ericka.arguedas@gmail.com>
2020-10-24 12:00:58 -0700

    About Component

--- src/components/DirectoryComponent.js
+++ src/components/DirectoryComponent.js
@@ -42,6 +42,4 @@ import { Link } from 'react-router-dom';
             );
         }
 
-
-
 export default Directory;
--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -35,7 +35,8 @@ class Header extends Component {
 
                 <Navbar dark sticky="top" expand="md">
                     <div className="container">
-                        <NavbarBrand className="mr-auto" href="/"><img src="/assets/images/logo.png" height="30" width="30" alt="NuCamp Logo" /></NavbarBrand>
+                        <NavbarBrand className="mr-auto" href="/">
+                            <img src="/assets/images/logo.png" height="30" width="30" alt="NuCamp Logo" /></NavbarBrand>
                         <NavbarToggler onClick={this.toggleNav} />
                         <Collapse isOpen={this.state.isNavOpen} navbar>
                             <Nav navbar>
