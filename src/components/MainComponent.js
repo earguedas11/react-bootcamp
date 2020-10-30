f18b77fb0061c53f4e26348772e3a4ca4aef9257
Ericka Arguedas <ericka.arguedas@gmail.com>
2020-10-29 20:59:58 -0700

    Intro to Redux

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -24,20 +24,18 @@ class Main extends Component {
 
         const HomePage = () => {
             return (
-                <Home 
+                <Home
                     campsite={this.props.campsites.filter(campsite => campsite.featured)[0]}
                     promotion={this.props.promotions.filter(promotion => promotion.featured)[0]}
                     partner={this.props.partners.filter(partner => partner.featured)[0]}
                 />
             );
-        }
+        };
 
         const CampsiteWithId = ({match}) => {
             return (
-                 <CampsiteInfo campsite={this.props.campsites.filter(campsite => campsite.id === 
-                    +match.params.campsiteId)[0]}
-                    comments={this.props.comments.filter(comment => comment.campsiteId ===
-                        +match.params.campsiteId)} />
+                <CampsiteInfo campsite={this.props.campsites.filter(campsite => campsite.id === +match.params.campsiteId)[0]} 
+                  comments={this.props.comments.filter(comment => comment.campsiteId === +match.params.campsiteId)} />
             );
         };
 
@@ -46,10 +44,10 @@ class Main extends Component {
                 <Header />
                 <Switch>
                     <Route path='/home' component={HomePage} />
-                    <Route exact path='/directory' render={() => <Directory campsites={this.state.campsites} />} />
-                    <Route path='directory/:camspiteId' component={CampsiteWithId} />
+                    <Route exact path='/directory' render={() => <Directory campsites={this.props.campsites} />} />
+                    <Route path='/directory/:campsiteId' component={CampsiteWithId} />
                     <Route exact path='/contactus' component={Contact} />
-                    <Route exact path='/aboutus' render={() => <About partners={this.state.partners} />} />
+                    <Route exact path='/aboutus' render={() => <About partners={this.props.partners} /> } />
                     <Redirect to='/home' />
                 </Switch>
                 <Footer />
